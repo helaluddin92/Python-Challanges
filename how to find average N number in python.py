0641cb6f7fd3a43f5a7b73bd24e13f4eb579ae96
Md. Helal Uddin <engr.helal92@gmail.com>
2020-04-24 13:44:08 +0600

    How to find average N number

--- how to find average N number in python.py
+++ how to find average N number in python.py
@@ -1,12 +1,14 @@
 # How to find average N number in python
 
-num = int(input("How many number? "))
-total_sum = 0
 
-for n in range(num):
-    number = int(input("Enter any number "))
-    total_sum += number
+def avg_n(num):
+    total_sum = 0
+    for n in range(num):
+        number = int(input("Enter any number "))
+        total_sum += number
+    avg = total_sum / num
+    return avg
 
-avg = total_sum/num
 
-print(avg)
+result = avg_n(int(input("How many number?")))
+print(result)
